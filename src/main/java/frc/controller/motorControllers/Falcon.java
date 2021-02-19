375dd6f774d695d6ce18e11f1338a3a0bfcbb959
Carter Harris <carter.50@hotmail.com>
2021-02-19 18:49:25 -0500

    Started adding falcon class

---
+++ src/main/java/frc/controller/motorControllers/Falcon.java
@@ -0,0 +1,30 @@
+package frc.controller.motorControllers;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import frc.controller.MotorController;
+
+public class Falcon implements MotorController {
+
+    private WPI_TalonFX talon; 
+
+    public Falcon() {
+
+    }
+
+    public void setSpeed(double speed) {
+    }
+
+    public void setRampRate(double rampRate) {
+
+    }
+
+    public void setCoastMode(boolean coast) {
+
+    }
+
+    public void follow(MotorController baseController, boolean invert) {
+
+    }
+}
